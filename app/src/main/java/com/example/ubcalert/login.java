eb5e7a00fd56a50e18ed3a13a3b942ab081e2b2b
Jayati <jae186@gmail.com>
2022-11-26 13:37:34 -0800

    added login page

---
+++ app/src/main/java/com/example/ubcalert/login.java
@@ -0,0 +1,34 @@
+package com.example.ubcalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class login extends AppCompatActivity {
+
+    String useruser;
+    EditText etuno;
+    Button buttonbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        buttonbutton = (Button) findViewById(R.id.button);
+        etuno = (EditText) findViewById(R.id.editTextTextPassword);
+
+        buttonbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                useruser = etuno.getText().toString();
+                Intent intentional = new Intent(login.this,MainActivity.class);
+                startActivity(intentional);
+            }
+
+        });
+
+    }
+}
